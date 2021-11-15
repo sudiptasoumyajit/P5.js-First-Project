d10548efc1b5922126c83f52e150cf6b3ac2261d
sudiptasoumyajit <84839625+sudiptasoumyajit@users.noreply.github.com>
2021-11-15 14:48:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
+noseX = 0;
+noseY = 0;
 function preload() {
-    //empty
+    clown_nose = loadImage("https://i.postimg.cc/Vsr2k2Dx/nose.png");
 }
 function setup() {
-    //canvas part
-    canvas = createCanvas(300, 300);
+    canvas = createCanvas(350, 350);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(350, 350);
+    video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
-function draw() {}
+function draw() {
+    image(video, 0, 0, 350, 350);
+    image(clown_nose, noseX, noseY, 30, 30);
+}
 function take_snapshot() {
     save ('FilteredFromJokerFileter.jpeg');
+}
+function modelLoaded() {
+    console.log('PoseNet Is Been Initialized');
+}
+function gotPoses(results) {
+    if (results.length>0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x-10;
+        noseY = results[0].pose.nose.y-10;
+    }
 }
